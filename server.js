44054cdcc174cdadd4b3f3faff47e47f6ac91279
ZambranoGalo001 <zambranogalo001@gmail.com>
2026-05-20 10:15:48 -0500

    Configuración inial del proyecto base

---
+++ server.js
@@ -0,0 +1,29 @@
+//Server.js
+const express = require('express');
+const cors = require('cors')
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+//Importación de rutas
+const authRoutes = require('./routes/auth');
+const usuariosRoutes = require('./routes/usuarios');
+
+//Usar rutas
+app.use('/api/auth', authRoutes)
+app.use('/api/usuarios', usuariosRoutes)
+
+//Ruta de ejemplo
+app.get('/', (req, res) => {
+    res.send('Hola desde el servidor express!');
+});
+
+//iniciar el servidor
+app.listen(port, () => {
+    console.log(`Servidor corriendo en el puerto ${port}`);
+});
